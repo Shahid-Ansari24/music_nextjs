2e50317136878dec0382e09684bf57b37663f946
Shahid <shahidansari2452@gmail.com>
2025-04-12 18:03:54 +0530

    feat: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-    <div className="h-100 text-2xl flex text-center justify-center items-center">
-      Hello World
-    </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <div className="h-100 text-2xl flex text-center justify-center items-center">
+        Hello World
+      </div>
+    </main>
   );
 }
